ce3ae38f145fc8e9d09eca470016dae23492f75d
Daniel Isaac-orji <loidbrix@gmail.com>
2025-10-29 02:11:21 +0100

    Integrate Book model with Django admin; customize list display, filters, and search

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # columns to display
+    list_filter = ('publication_year', 'author')  # add filters in the sidebar
+    search_fields = ('title', 'author')  # enable search by title or author
