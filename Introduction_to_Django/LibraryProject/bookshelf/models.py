ad349fb796e9504409dcb00195d9152cd122fd2d
Daniel Isaac-orji <loidbrix@gmail.com>
2025-10-29 02:13:42 +0100

    Integrate Book model with Django admin; customize list display, filters, and search

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -6,4 +6,4 @@ class Book(models.Model):
     publication_year = models.IntegerField()
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.title
