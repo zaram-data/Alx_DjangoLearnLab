cd8cedbbad9effe2556fd9d8bd2608b4e5b89ce7
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-07 16:30:46 +0100

    Fix imports and URL patterns for function-based and class-based views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # Include app URLs
+    path('books/', list_books, name='book-list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
 ]
