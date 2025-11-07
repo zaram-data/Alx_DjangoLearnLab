6502339804d76a04399e5d6c40f2cca23e39f1f9
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-07 16:08:47 +0100

    Remove incorrect git file and fix folder structure

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,16 @@
+from .models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
+
+# List all books in a library
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
