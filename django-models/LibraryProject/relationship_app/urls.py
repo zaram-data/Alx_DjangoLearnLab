7fb573f554516dd958ba58470a96aa18cda0e9ee
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-10 11:51:22 +0100

    Fix DetailView import to satisfy ALX checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
-ffrom django.urls import path
-from .views import list_books, LibraryDetailView
+from django.urls import path
+from .views import list_books, LibraryDetailView, register_view, login_view, logout_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
