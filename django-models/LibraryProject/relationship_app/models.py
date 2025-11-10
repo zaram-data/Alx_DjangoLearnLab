219211011fc7f3ee9686597ab5be5ffd8f1a9b11
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-10 11:17:42 +0100

    Add specific import for DetailView to satisfy ALX checker

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,16 +1,33 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,14 @@
-from .models import Author, Book, Library, Librarian
+from .models import Book, Library, Author, Librarian
 
-# Query all books by a specific author
-def get_books_by_author(author_name):
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = Book.objects.filter(library=library)
+    return books
+
+def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
 
-# List all books in a library
-def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
-
-# Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
+def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return Librarian.objects.get(library=library)
