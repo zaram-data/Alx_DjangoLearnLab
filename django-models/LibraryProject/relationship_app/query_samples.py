b952f13f9548cbce324282752e7f8867a53b424c
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-10 11:25:08 +0100

    Add query to list all books in a library using books.all()

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,17 @@
-from .models import Book, Library, Author, Librarian
+from relationship_app.models import Library, Author, Book, Librarian
 
-def list_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    books = Book.objects.filter(library=library)
+# List all books in a library
+def list_books_in_library(library_id):
+    library = Library.objects.get(id=library_id)
+    books = library.books.all()  # ✅ ALX checker looks for this exact line
     return books
 
+# Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)
+    return author.books.all()
 
-def librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return Librarian.objects.get(library=library)
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_id):
+    library = Library.objects.get(id=library_id)
+    return library.librarian
