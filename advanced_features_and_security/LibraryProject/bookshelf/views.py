918529aad6af77fed8917b0a4b96b28bdb7a1c36
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-13 11:22:05 +0100

    Move settings and core files to LibraryProject/LibraryProject to match ALX checker

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,16 +2,25 @@
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
-from .models import Book
-from .forms import ExampleForm  # <- Add this line
+from .models import Book  # Make sure you have a Book model in models.py
+from .forms import ExampleForm
 
 # View for listing books with proper permission handling
 @permission_required('bookshelf.view_book', raise_exception=True)
 def book_list(request):
-    # Fetch all Book objects
     books = Book.objects.all()
-    
-    # Pass the books to the template
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
+# View to render the ExampleForm
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Handle form submission here (e.g., save data, process input)
+            # For now, just re-render the form after submission
+            return render(request, 'bookshelf/form_example.html', {'form': form, 'success': True})
+    else:
+        form = ExampleForm()
+    
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
