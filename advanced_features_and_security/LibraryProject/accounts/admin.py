e902df09f6bda613849238a51c99fe4a85a0e83b
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-13 09:39:08 +0100

    Implemented custom user model and admin integration

---
+++ advanced_features_and_security/LibraryProject/accounts/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- django-models/LibraryProject/relationship_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class RelationshipAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LibraryProject.relationship_app'
