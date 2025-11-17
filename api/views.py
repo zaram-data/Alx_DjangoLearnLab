fd38d7c25993e76b8a49f51945f01c7522b0c371
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-17 12:42:52 +0100

    chore: initial Django + DRF project (api_project, api app, Book model)

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
