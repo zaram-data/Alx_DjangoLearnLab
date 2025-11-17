ce267060736c559b3a6255d0ca57350f06efc0e7
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-17 13:57:58 +0100

    feat: add token authentication and IsAuthenticated permission

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',    
-    'api',
+    'rest_framework',
+    'rest_framework.authtoken',  # Add this line
+    'api'
 ]
 
 MIDDLEWARE = [
