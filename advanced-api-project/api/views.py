a69fde67d8976bf1eefccb9d20e759ce1885e769
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-25 18:06:38 +0100

    fix: import DRF permission classes to satisfy checker

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Book
