99aed1bdec6e83a7cd6e2f73d1dfa55e4c90207e
Daniel Isaac-orji <loidbrix@gmail.com>
2025-11-25 17:14:28 +0100

    feat: setup project with custom serializers

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for api_project project.
+URL configuration for advanced_api_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  # make sure include is imported
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # ← this connects your api app
 ]
--- advanced-api-project/advanced_api_project/wsgi.py
+++ advanced-api-project/advanced_api_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for api_project project.
+WSGI config for advanced_api_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced_api_project.settings')
 
 application = get_wsgi_application()
