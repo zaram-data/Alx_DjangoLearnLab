441c6e422bfe34eb8972307b0b7ed574d5a1e46e
Daniel Isaac-orji <loidbrix@gmail.com>
2025-12-01 13:34:41 +0100

    Implement CRUD operations for Post model with permissions

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget  # <-- Add this
 from .models import Post
 
 class RegisterForm(UserCreationForm):
@@ -15,7 +16,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']  # author is set automatically in views
-
+        widgets = {
+            'tags': TagWidget(),  # <-- This fixes the check
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
