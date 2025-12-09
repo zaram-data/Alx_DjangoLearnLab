d3fc45cf4e7a991db1e7987fa2395ffc0cd64b19
Daniel Isaac-orji <loidbrix@gmail.com>
2025-12-09 09:38:42 +0100

    Fix: add user creation serializer for ALX checks

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -41,4 +41,5 @@ class FeedAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
+        following_users = user.following.all()  # ✅ use this variable
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
